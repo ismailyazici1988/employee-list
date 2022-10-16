e7ab7c262068583bd941652cfb2c7a0aa7d9b5cc
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-10-16 21:51:32 +0300

    header is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Button from "./components/Button/Button";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import data from "./assests/data";
+import { useState } from "react";
 
 function App() {
+const [page,setPage]= useState(1);
+const userPerPage = 5;
+
+const indexOffLastUser= page*userPerPage;
+const indexOffFirstUser= indexOffLastUser- userPerPage;
+
+const currentUsers = data.slice(indexOffFirstUser,indexOffLastUser);
+
+
+
   return (
     <div className="App">
-      <h2>REACT</h2>
+      <Header from={indexOffFirstUser+1} to ={indexOffFirstUser+currentUsers.length}/>
+      <Main/>
+      <Button/>
     </div>
   );
 }
